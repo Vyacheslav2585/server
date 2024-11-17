ddc9c75cc1de68493053157e2ce0bba7a07c6ef5
Vyacheslav <vyacheslavs010s@gmail.com>
2024-11-17 22:51:33 +0200

    gg

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const http = require('http').Server(app)
+const cors = require('cors')
+const SocketIo = require('socket.io')
+
+app.use(cors())
+
+const socket =SocketIo(http,{
+    cors:{
+        origit:"https://localhost:5173"
+    }
+})
+let arr = []
+socket.on('connection',(socket)=>{
+    
+    arr.push(`${socket.id}`)
+    console.log(arr)
+    socket.on('message',(item)=>{
+        console.log(item)
+    })
+    socket.on('disconnect',(client)=>{
+        arr = arr.filter((item)=>item!=socket.id)
+        console.log(`user ${socket.id} disconnected`)
+    })
+})
+app.get('/api',(req,res)=>{
+    res.json({
+        message:'Hello world'
+    })
+})
+http.listen(port,()=>{
+    console.log(`Server listening on ${port}`);
+})
